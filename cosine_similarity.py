6482bc5cd815ff9d0908c7dc1edb8da77e9afc3e
Oskar Fraser-Krauss <oskarfkrauss@gmail.com>
2025-11-03 14:54:04 +0000

    setup repo

---
+++ cosine_similarity.py
@@ -0,0 +1,15 @@
+from sklearn.metrics.pairwise import cosine_similarity
+
+# data from Jonah
+staph_person_1 = 'ACTGTTTGGAACCC'
+staph_person_2 = 'ACTGTTTGGATTTT'
+
+# embedding_person_1 = transformer_2(staph_person_1)
+# embedding_person_2 = transformer_2(staph_person_2)
+
+# turn these samples into a list of numbers
+embedding_person_1 = [[1, 2, 3], [4, 5, 6]]
+embedding_person_2 = [[6, 2, 3], [4, 5, 1]]
+
+# how similar are these two lists of numbers
+metric = cosine_similarity(embedding_person_1, embedding_person_2)
