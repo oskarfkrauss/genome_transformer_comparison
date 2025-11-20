206cc8e515d02ca03c41018a305e45b5172bf097
Oskar Fraser-Krauss <oskarfkrauss@gmail.com>
2025-11-20 15:13:37 +0000

    add plasmid embedding logic

--- genome_transformer_comparison/generate_embeddings.py
+++ genome_transformer_comparison/generate_embeddings.py
@@ -29,19 +29,35 @@ kmer_length = len(first_kmer)
 max_seq_length = kmer_length * (tokenizer.model_max_length - 1)
 
 # get paths to folders in input
-bacteria_names = os.listdir(os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['cpes_or_imps']))
+bacteria_names = os.listdir(
+    os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['whole_genomes_or_plasmids'],
+                 EMBEDDING_CONFIG['cpes_or_imps']))
 
 for bacteria_name in bacteria_names:
     # TODO: setup logging
-    print(bacteria_name)
+    if EMBEDDING_CONFIG['whole_genomes_or_plasmids'] == 'whole_genomes':
+        # then the folder structure is broken down by bacteria
+        fasta_file_names = os.listdir(
+            os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['whole_genomes_or_plasmids'],
+                         EMBEDDING_CONFIG['cpes_or_imps'], bacteria_name))
 
-    fasta_file_names = os.listdir(
-        os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['cpes_or_imps'], bacteria_name))
+        fasta_file_paths = [
+            os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['whole_genomes_or_plasmids'],
+                         EMBEDDING_CONFIG['cpes_or_imps'], bacteria_name, file_name)
+            for file_name in fasta_file_names
+        ]
+    else:
+        # then the folder structure is not broken down by bacteria
+        fasta_file_names = os.listdir(
+            os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['whole_genomes_or_plasmids'],
+                         EMBEDDING_CONFIG['cpes_or_imps']))
 
-    fasta_file_paths = [
-        os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['cpes_or_imps'], bacteria_name, file_name)
-        for file_name in fasta_file_names
-    ]
+        fasta_file_paths = [
+            os.path.join(ROOT_DIR, 'inputs', EMBEDDING_CONFIG['whole_genomes_or_plasmids'],
+                         EMBEDDING_CONFIG['cpes_or_imps'], file_name)
+            for file_name in fasta_file_names
+        ]
+        # NOTE: this is a not a great solution but can't really be avoided atm, sorry
 
     for i, fasta_file in enumerate(fasta_file_paths):
         start_time = time.perf_counter()
--- genome_transformer_comparison/generate_embeddings_config.py
+++ genome_transformer_comparison/generate_embeddings_config.py
@@ -1,7 +1,9 @@
 '''this should be a yaml file'''
 EMBEDDING_CONFIG = {
+    # wgs or plasmids
+    'whole_genomes_or_plasmids': 'plasmids',
     # whether we are trying to embed the CPE sequences or IMPs
-    'cpes_or_imps': 'cpes',
+    'cpes_or_imps': 'imps',
     # which pre trained transformer model to use, can be
     # NucleotideTransformer_2.5B, DNABERT_S, HyenaDNA_medium_160k, or GENA_LM_base, currently
     # only workd for NucleotideTransformer_2.5B
